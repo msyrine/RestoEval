4c045e37ecb06793a9b4e1c53c82a8af9c48cefa
Methlouthi Syrine <methlouthisyrine@Methlouthis-Mac.local>
2017-12-05 23:26:17 +0100

    Initial Commit

---
+++ Test2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test2
+//
+//  Created by Methlouthi Syrine on 12/5/17.
+//  Copyright © 2017 Methlouthi Syrine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
